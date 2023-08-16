1f060348c6fdb2c0e23107d8bc76e9e26da87fbd
annassiddique <117367098+annassiddique@users.noreply.github.com>
2023-08-16 17:54:50 +0500

    Committing modified files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import Collections from "./components/pages/Collections";
+import Cart from "./components/pages/Cart";
+import Footer from "./components/Footer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Details from "./components/pages/Details";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/collection" element={<Collections  />} />
+        <Route exact path="/collection/:Category/:id" element={<Details />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer/>
+    </Router>
+
+    </Provider>
   );
 }
 
